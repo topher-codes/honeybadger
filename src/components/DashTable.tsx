f3813009923489c9c85de717e8988d543024f451
Chris <ghost@drednaut.net>
2023-01-03 11:15:28 -0800

    init
    Signed-off-by: Chris <ghost@drednaut.net>

---
+++ src/components/DashTable.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const DashTable: React.FC = () => {
+  return (
+    <>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Job</th>
+            <th className="px-4 py-2">Favorite Color</th>
+            <th className="px-4 py-2">Pets</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border px-4 py-2">John Doe</td>
+            <td className="border px-4 py-2">Software Engineer</td>
+            <td className="border px-4 py-2">Blue</td>
+            <td className="border px-4 py-2">Cat</td>
+          </tr>
+          <tr className="bg-gray-100">
+            <td className="border px-4 py-2">Jane Doe</td>
+            <td className="border px-4 py-2">Product Manager</td>
+            <td className="border px-4 py-2">Green</td>
+            <td className="border px-4 py-2">Dog</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default DashTable;
